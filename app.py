a844b9dcb8b5556c63a1e3a99666cfa8db9b6602
Alejandro Roca <alekss.ro@gmail.com>
2019-09-05 19:26:23 +0200

    debug

--- app.py
+++ app.py
@@ -2,14 +2,29 @@
 # import sys
 from helpers import ReadLines
 from helpers import Entry
+from helpers import Model
 
 infile = "Meteologica_vacante_ProgC_Problema_20190903/Meteologica_vacante_ProgC_ProblemaDatos_20190903.txt"
 
 data = ReadLines(infile)
 
-entry_try = Entry(data.predictions[1])
+print(len(data.all_entries))
+print(len(data.observations))
+print(len(data.predictions))
 
-# print(entry_try.date)
-# print(entry_try.time)
-# print(entry_try.energy_production)
-# print(entry_try.wind_speed)
+# transform arrays to 'Entry' objects
+observations = []
+predictions = []
+for obs in data.observations:
+    observations.append(Entry(obs))
+for pred in data.predictions:
+    predictions.append(Entry(pred))
+
+m = Model(observations, predictions)
+for pred in m.predict_on_ord:
+    print(pred.date, pred.time, pred.energy_production, pred.wind_speed)
+
+print(m.fitted_model[0])
+print(m.fitted_model[1])
+
+print(m.predicts)
